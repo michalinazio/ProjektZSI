8d2d5d03086c9ea2b9aa5f73057bf5ed64489d26
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-05-03 17:26:14 +0200

    Próba naprawy problemów z polskim znakami

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ def get_db():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        charset="utf8mb4"
     )
     
 @app.get("/api/lizards")
