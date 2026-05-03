1a6f7c08d1e86247b0dfa68aad9a93e55e61b738
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-05-03 16:37:32 +0200

    funkcja get_lizards która zwraca nam tablicę lizards(jaszczurki)

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,13 @@ def get_db():
         password=os.getenv("DB_PASS"),
         database=os.getenv("DB_NAME")
     )
+    
+@app.get("/api/lizards")
+def get_lizards():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM lizards")
+    return cursor.fetchall()
 
 @app.get("/")
 def home():
