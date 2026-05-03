c8a3f1f2589775449f9cb2877a277ec593b6558b
InuTheGreat <konrad.mrozowskii@gmail.com>
2026-05-03 17:23:38 +0200

    Próba naprawy problemów z polskim znakami

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,6 +2,6 @@ CREATE TABLE lizards (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     votes INT DEFAULT 0
-);
+)CHARACTER SET utf8mb4;
 
 INSERT INTO lizards (name) VALUES ('Jaszczurka Zwinka'), ('Jaszczurka Zielona'), ('Jaszczurka Żyworodna');
