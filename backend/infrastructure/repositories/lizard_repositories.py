2e96ba42fd3d99165c06a84d28d5c80099fbb3c1
DafitS <dw220105@mail.com>
2026-06-01 23:00:24 +0200

    Added new function for repository lizard

--- backend/infrastructure/repositories/lizard_repositories.py
+++ backend/infrastructure/repositories/lizard_repositories.py
@@ -11,4 +11,33 @@ def get_lizard_by_id(self, lizard_id: int):
     if lizard_found_model is None:
         return None
     return LizardMapper.to_domain(lizard_found_model)
+
+def get_all_lizards(self):
+    lizard_models = self.db_session.query(LizardModel).all()
+    return [LizardMapper.to_domain(model) for model in lizard_models]
+
+def create_liazard(self, lizard_data):
+    new_lizard_model = LizardModel(
+        name = lizard_data.name,
+        age = lizard_data.age,
+        species_id = lizard_data.species_id
+    )
+
+    self.db_session.add(new_lizard_model)
+    self.db_session.commit()
+    self.db_session.refresh(new_lizard_model)
+
+    return LizardMapper.to_domain(new_lizard_model)
+
+def delete_lizard(self, lizard_id: int):
+    lizard_to_delete = self.db.session.query(LizardModel).filter(LizardModel.id == lizard_id).first()
+    if lizard_to_delete is None:
+        return False
+    self.db_session.delete(lizard_to_delete)
+    self.db_session.commit()
+    self.db_session.refresh(lizard_to_delete)
+    return True
+
+
+
     
