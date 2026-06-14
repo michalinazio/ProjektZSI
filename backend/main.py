b12936b22e1883714dcb2d2888b4c8bf68a7e7cf
michalinazio <michalina.ziolkowska1202@gmail.com>
2026-06-14 09:12:34 +0200

    Update main.py
    Import fastapi

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from backend.application.api.routers.lizard_vote_router import router as lizard_vote_router
 from backend.application.api.routers.lizard_router import router as lizard_router
 from backend.application.api.routers.species_router import router as species_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Lizard API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8080"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(lizard_router)
 app.include_router(species_router)
